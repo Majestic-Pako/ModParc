092c00a32d2c09685961458613f9e140c9fd87a3
Majestic-Pako <agustinchoque484@gmail.com>
2024-09-11 12:56:09 -0300

    Actu
    Ya pude mover el Validar nombre a la clase Vtv cambie el mensaje de entrada por uno mas amigable opcional mejorar los terminos de ingreos de autos y cambiar el yes o no

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,50 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-
+		String datos = "";
 		Vtv vehiculo = new Vtv("");
 		Vtv Motor = new Vtv("");
 		Vtv Frenos = new Vtv("");
 		Vtv Suspension = new Vtv("");
 		int opcion;
 		String auto;
-		String motor;
-		String frenos;
-		String suspension;
-		JOptionPane.showMessageDialog(null,
-				"Bienvenido a la VTV ingrese el vehiculo \n Ingrese como se encuentra el estado del auto");
+		JOptionPane.showMessageDialog(null, "¡Bienvenido al Sistema de Verificación Técnica Vehicular (VTV)!\n"
+				+ "Aquí podrá registrar su vehículo y realizar las pruebas correspondientes de manera rápida y sencilla.");
 		do {
-			String convertir[] = { "Registro de autos", "Inspeccion", "Salir" };
+			String convertir[] = { "Ultimo Registro", "Inspeccion", "Salir" };
 			opcion = JOptionPane.showOptionDialog(null, "Seleccione una opcion", null, 0, 0, null, convertir,
 					convertir[0]);
 			switch (opcion) {
 			case 0:
-				JOptionPane.showMessageDialog(null, "El Vehiculo: "+vehiculo.getAutos()+
-												"\nInforme: " +vehiculo.getRegistro());
+				if (datos.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Todavia no has registrado ningun Vehiculo");
+				} else {
+					JOptionPane.showMessageDialog(null, datos);
+				}
 				break;
 			case 1:
 				do {
 					auto = JOptionPane.showInputDialog("Ingresa la marca del auto");
-				} while (!ValidarNombre(auto));
-				vehiculo.setAuto(auto);
+					vehiculo.setAuto(auto);
+				} while (!vehiculo.ValidarNombre());
 				JOptionPane.showMessageDialog(null, "Vehiculo registrado  " + vehiculo.getAutos());
-				motor = Vtv.Elecciones("Motor");
+				String motor = Vtv.Elecciones("Motor");
 				Motor.setDatos(motor);
 
-				frenos = Vtv.Elecciones("frenos");
+				String frenos = Vtv.Elecciones("frenos");
 				Frenos.setDatos(frenos);
 
-				suspension = Vtv.Elecciones("Sistema de Suspension");
+				String suspension = Vtv.Elecciones("Sistema de Suspension");
 				Suspension.setDatos(suspension);
 
 				String res = "Vehículo " + vehiculo.getAutos() + "\nPruebas Realizadas: " + "\nEstado de motor: "
 						+ Motor.getDatos() + "\nEstado de Frenos: " + Frenos.getDatos() + "\nEstado de Suspension: "
 						+ Suspension.getDatos();
 				JOptionPane.showMessageDialog(null, res);
-				
+
 				String fin = vehiculo.Final(motor, frenos, suspension);
 				JOptionPane.showMessageDialog(null, fin);
+				datos = "Vehiculo: " + vehiculo.getAutos() + "\nInforme: " + vehiculo.getRegistro();
 				break;
 			case 2:
 				JOptionPane.showMessageDialog(null, "Informe Terminado. Hasta pronto.");
@@ -54,19 +55,4 @@ public class Main {
 		} while (opcion != 2);
 	}
 
-	public static boolean ValidarNombre(String auto) {
-		if (auto.isEmpty() || auto.length() < 3 || auto.length() > 25) {
-			JOptionPane.showMessageDialog(null, "ERROR en la Cantidad de caracteres");
-			return false;
-		} else {
-			for (int i = 0; i < auto.length(); i++) {
-				if (Character.isDigit(auto.charAt(i))) {
-					JOptionPane.showMessageDialog(null, "Hay un nùmero");
-					return false;
-				}
-			}
-			JOptionPane.showMessageDialog(null, "Marca valida");
-			return true;
-		}
-	}
 }
--- src/Vtv.java
+++ src/Vtv.java
@@ -14,15 +14,16 @@ public class Vtv {
 
 	public void AgReg(String registro) {
 		LocalDate fecha = LocalDate.now();
-		this.registro = registro + auto + "\nPaso la Vtv "+ "\nEl dia " + fecha + 
-						"\nProxima inspeccion: " + fecha.plusYears(1) ;
+		this.registro = registro + auto + "\nPaso la Vtv " + "\nEl dia " + fecha + "\nProxima inspeccion: "
+				+ fecha.plusYears(1);
 	}
-	
+
 	public void DegReg(String registro) {
 		LocalDate fecha = LocalDate.now();
-		this.registro = registro + auto + "\nNo paso la Vtv " + "\n El dia " + fecha +
-						"\nTiene un lapso hasta " + fecha.plusMonths(1) + "\nPara volver a intentarlo sin coste nuevo";
+		this.registro = registro + auto + "\nNo paso la Vtv " + "\n El dia " + fecha + "\nTiene un lapso hasta "
+				+ fecha.plusMonths(1) + "\nPara volver a intentarlo sin coste nuevo";
 	}
+
 	public String getRegistro() {
 		return this.registro;
 	}
@@ -55,13 +56,30 @@ public class Vtv {
 		}
 
 	}
+
 	public String Final(String motor, String frenos, String suspension) {
-		if(motor.equals("Aceptado") && frenos.equals("Aceptado") && suspension.equals("Aceptado")) {
+		if (motor.equals("Aceptado") && frenos.equals("Aceptado") && suspension.equals("Aceptado")) {
 			this.AgReg("El vehiculo: ");
 			return this.getRegistro();
-		}else {
+		} else {
 			this.DegReg("El vehiculo: ");
 			return this.getRegistro();
+		}
+	}
+
+	public boolean ValidarNombre() {
+		if (auto.isEmpty() || auto.length() < 3 || auto.length() > 25) {
+			JOptionPane.showMessageDialog(null, "ERROR en la Cantidad de caracteres");
+			return false;
+		} else {
+			for (int i = 0; i < auto.length(); i++) {
+				if (Character.isDigit(auto.charAt(i))) {
+					JOptionPane.showMessageDialog(null, "Hay un nùmero");
+					return false;
+				}
+			}
+			JOptionPane.showMessageDialog(null, "Marca valida");
+			return true;
 		}
 	}
 
