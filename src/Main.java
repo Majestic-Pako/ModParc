e62670f0f0c0461a75d328647e45248201ac83a5
Majestic-Pako <agustinchoque484@gmail.com>
2024-09-11 00:15:07 -0300

    Detalles de opciones
    Codigo mas ordenado con los espacios ya declarados en los mensajes las opciones falta mandar los datos a revision y determinar si cumple los parametros de inspeccion

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,54 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Vtv vehiculo = new Vtv("");
 		Vtv Motor = new Vtv("");
 		Vtv Frenos = new Vtv("");
+		Vtv Suspension = new Vtv("");
 		int opcion;
 		String auto;
 		String motor;
 		String frenos;
-		JOptionPane.showMessageDialog(null, "Bienvenido a la VTV ingrese el vehiculo \n Ingrese como se encuentra el estado del auto");
+		String suspension;
+		JOptionPane.showMessageDialog(null,
+				"Bienvenido a la VTV ingrese el vehiculo \n Ingrese como se encuentra el estado del auto");
 		do {
-			String convertir[]= {"Registro de autos", "Inspeccion", "Salir"};
+			String convertir[] = { "Registro de autos", "Inspeccion", "Salir" };
 			opcion = JOptionPane.showOptionDialog(null, "Seleccione una opcion", null, 0, 0, null, convertir,
 					convertir[0]);
-			switch(opcion){
+			switch (opcion) {
 			case 0:
 				JOptionPane.showMessageDialog(null, "a");
 				break;
 			case 1:
 				do {
-					 auto = JOptionPane.showInputDialog("Ingresa la marca del auto");
-				}while(!ValidarNombre(auto));
+					auto = JOptionPane.showInputDialog("Ingresa la marca del auto");
+				} while (!ValidarNombre(auto));
 				vehiculo.setAuto(auto);
-				JOptionPane.showMessageDialog(null, "Vehiculo registrado  "+ vehiculo.getAutos());
+				JOptionPane.showMessageDialog(null, "Vehiculo registrado  " + vehiculo.getAutos());
 				motor = Vtv.Elecciones("Motor");
-                Motor.setDatos(motor);
-                
-                frenos = Vtv.Elecciones("frenos");
-                Frenos.setDatos(frenos);
-                String res = "El vehículo " + vehiculo.getAutos() + " tiene un estado de motor: " + Motor.getDatos() + "y"+Frenos.getDatos();
-                JOptionPane.showMessageDialog(null, res);
-                
+				Motor.setDatos(motor);
+
+				frenos = Vtv.Elecciones("frenos");
+				Frenos.setDatos(frenos);
+
+				suspension = Vtv.Elecciones("Sistema de Suspension");
+				Suspension.setDatos(suspension);
+
+				String res = "Vehículo " + vehiculo.getAutos() + "\nPruebas Realizadas: " + "\nEstado de motor: "
+						+ Motor.getDatos() + "\nEstado de Frenos: " + Frenos.getDatos() + "\nEstado de Suspension "
+						+ Suspension.getDatos();
+				JOptionPane.showMessageDialog(null, res);
+
 				break;
-			case 2: 
-				JOptionPane.showMessageDialog(null, "c");
+			case 2:
+				JOptionPane.showMessageDialog(null, "Adios...");
 				break;
 			}
-		}while(opcion != 2);
+		} while (opcion != 2);
 	}
+
 	public static boolean ValidarNombre(String auto) {
 		if (auto.isEmpty() || auto.length() < 3 || auto.length() > 25) {
 			JOptionPane.showMessageDialog(null, "ERROR en la Cantidad de caracteres");
--- src/Vtv.java
+++ src/Vtv.java
@@ -1,43 +1,52 @@
 import java.time.LocalDate;
 
 import javax.swing.JOptionPane;
+
 public class Vtv {
 	private String auto;
 	private String datos;
 	private String registro;
-	
+
 	public Vtv(String auto) {
 		this.auto = auto;
 		this.registro = "";
 	}
+
 	public void AgReg(String registro) {
 		this.registro = registro + auto + " Registrado  " + LocalDate.now();
 	}
-	public String getRegistro(){
+
+	public String getRegistro() {
 		return this.registro;
 	}
+
 	public String getAutos() {
 		return this.auto;
 	}
+
 	public String getDatos() {
 		return this.datos;
 	}
+
 	public void setDatos(String datos) {
 		this.datos = datos;
 	}
+
 	public void setAuto(String auto) {
 		this.auto = auto;
 	}
+
 	public static String Elecciones(String seccion) {
-		 int x = JOptionPane.showConfirmDialog(null, "Indique el resulatdo de la prueba "+ seccion + " ¿Estado?",seccion, JOptionPane.YES_NO_CANCEL_OPTION);
-		 if (x == JOptionPane.YES_OPTION) {
-	            return "Aceptado";
-	        } else if (x == JOptionPane.NO_OPTION) {
-	        	return "Rechazado";
-	        } else {
-	        	return "No se selecciono ninguna opcion";
-	        }
-		
+		int x = JOptionPane.showConfirmDialog(null, "Indique el resulatdo de la prueba " + seccion, seccion,
+				JOptionPane.YES_NO_CANCEL_OPTION);
+		if (x == JOptionPane.YES_OPTION) {
+			return "Aceptado";
+		} else if (x == JOptionPane.NO_OPTION) {
+			return "Rechazado";
+		} else {
+			return "No se selecciono ninguna opcion";
+		}
+
 	}
-	
+
 }
